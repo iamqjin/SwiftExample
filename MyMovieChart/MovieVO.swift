af243306c395c1310c6e3856c14ea31cfc23fc06
iamqjin <iamqjin@gmail.com>
2017-08-16 16:39:27 +0900

    First Commit Swift Example

---
+++ MyMovieChart/MovieVO.swift
@@ -0,0 +1,24 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by iamqjin on 2017. 8. 16..
+//  Copyright © 2017년 iamqjin. All rights reserved.
+//
+
+import Foundation
+
+class MovieVO {
+    
+    var thumbnail : String? //영화섬네일 이미지 주소
+    
+    var title : String? // 영화 제목
+    
+    var description : String? //영화 설명
+    
+    var detail : String? //상세정보
+    
+    var opendate : String? //개봉일
+    
+    var rating : Double? //평점
+}
